016117ddf859f8153086f4538c716f7e1b901216
galih <samudragalih94@gmail.com>
2024-02-26 19:12:21 +0700

    wip start project

---
+++ RockPaperScissor/RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Galih Samudra on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
