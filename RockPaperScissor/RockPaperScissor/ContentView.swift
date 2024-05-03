1e2d1908105827c56fa7c8680ad2c09b32c6cad8
galih <samudragalih94@gmail.com>
2024-05-03 14:57:10 +0700

    feat(UI): implement game rock paper scissors

--- RockPaperScissor/RockPaperScissor/ContentView.swift
+++ RockPaperScissor/RockPaperScissor/ContentView.swift
@@ -7,15 +7,154 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+enum MoveResult {
+    case win
+    case lose
+    case draw
+}
+
+enum Move {
+    case paper, rock, scissor
+    
+    static func calculateMoveResult(playerMove: Move, enemyMove: Move) -> MoveResult {
+        switch (playerMove, enemyMove) {
+        case (.paper, .rock), (.scissor, .paper), (.rock, .scissor) :
+            return .win
+        case (.paper, .scissor), (.scissor, .rock), (.rock, .paper):
+            return .lose
+        default:
+            return .draw
         }
-        .padding()
+    }
+    
+    func getImage() -> String {
+        switch self {
+        case .paper:
+            return "newspaper"
+        case .rock:
+            return "mountain.2.fill"
+        case .scissor:
+            return "scissors"
+        }
+    }
+    
+    func getName() -> String {
+        switch self {
+        case .paper:
+            return "Paper"
+        case .rock:
+            return "Rock"
+        case .scissor:
+            return "Scissors"
+        }
+    }
+}
+
+struct ContentView: View {
+    let listOfMoves: [Move] = [.paper, .rock, .scissor]
+    @State private var enemyMoveIndex = Int.random(in: 0...2)
+    @State private var winScore = 0
+    @State private var loseScore = 0
+    @State private var drawScore = 0
+    @State private var buttonTitle = ""
+    @State private var scoreTitle = ""
+    @State private var showingScore = false
+    private let MAX_QUESTION = 10
+    
+    var body: some View {
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.5), location: 0.7),
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.3), location: 0.7)
+            ], center: .top, startRadius: 100, endRadius: 500)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Text("Rock Paper Scissors")
+                    .foregroundStyle(.white)
+                    .font(.largeTitle.bold())
+                
+                VStack(spacing: 15) {
+                    Text("Choose your move")
+                        .foregroundStyle(.white)
+                        .font(.largeTitle.bold())
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            chooseMove(number)
+                        } label: {
+                            Image(systemName: listOfMoves[number].getImage())
+                                .resizable()
+                                .frame(width: 50, height: 40)
+//                            Image(listOfMoves[number].getImage())
+                        }
+                        
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 50))
+                Spacer()
+                Spacer()
+                VStack(spacing: 5) {
+                    Text("WIN: \(winScore)")
+                        .foregroundStyle(.white)
+                        .font(.largeTitle.bold())
+                    Text("LOSE: \(loseScore)")
+                        .foregroundStyle(.white)
+                        .font(.largeTitle.bold())
+                    Text("DRAW: \(drawScore)")
+                        .foregroundStyle(.white)
+                        .font(.largeTitle.bold())
+                }
+                Spacer()
+            }
+            .padding()
+        }.alert(scoreTitle, isPresented: $showingScore) {
+            Button(buttonTitle) {
+                nextRound()
+            }
+        } message: {
+            Text("Currrent score is \(winScore) win, \(loseScore) lose, and \(drawScore) draw")
+        }
+    }
+    
+    private func chooseMove(_ number: Int) {
+        let numberOfQuestion = 1 + winScore + loseScore + drawScore
+        
+        let playerMove = listOfMoves[number]
+        enemyMoveIndex = Int.random(in: 0...2)
+        let enemyMove = listOfMoves[enemyMoveIndex]
+        let result = Move.calculateMoveResult(playerMove: playerMove, enemyMove: enemyMove)
+        
+        scoreTitle = "You choose \(playerMove.getName()) and your enemy choose \(enemyMove.getName()), "
+        if numberOfQuestion == MAX_QUESTION {
+            buttonTitle = "Play Again"
+        } else {
+            buttonTitle = "Continue"
+        }
+        switch result {
+        case .win:
+            scoreTitle += "You Win"
+            winScore += 1
+        case .lose:
+            scoreTitle += "You Lose"
+            loseScore += 1
+        case .draw:
+            scoreTitle += "It's a Draw"
+            drawScore += 1
+        }
+        showingScore = true
+    }
+    private func nextRound() {
+        let numberOfQuestion = 1 + winScore + loseScore + drawScore
+        if numberOfQuestion == MAX_QUESTION {
+            winScore = 0
+            loseScore = 0
+            drawScore = 0
+        }
     }
 }
 
